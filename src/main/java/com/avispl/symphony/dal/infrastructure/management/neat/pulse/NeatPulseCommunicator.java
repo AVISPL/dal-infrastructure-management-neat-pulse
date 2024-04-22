eca5d5da6bc9ec590e03bd1c83df7e6c83965530
HieuLM-AVISPL <hm15015@avispl.com>
2024-04-22 10:42:18 +0700

    SYAL-draft-code draft code for adapter

---
+++ src/main/java/com/avispl/symphony/dal/infrastructure/management/neat/pulse/NeatPulseCommunicator.java
@@ -0,0 +1,348 @@
+/*
+ *  Copyright (c) 2024 AVI-SPL, Inc. All Rights Reserved.
+ */
+
+package com.avispl.symphony.dal.infrastructure.management.neat.pulse;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.CollectionUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.avispl.symphony.api.dal.control.Controller;
+import com.avispl.symphony.api.dal.dto.control.ControllableProperty;
+import com.avispl.symphony.api.dal.dto.monitor.ExtendedStatistics;
+import com.avispl.symphony.api.dal.dto.monitor.Statistics;
+import com.avispl.symphony.api.dal.dto.monitor.aggregator.AggregatedDevice;
+import com.avispl.symphony.api.dal.monitor.Monitorable;
+import com.avispl.symphony.api.dal.monitor.aggregator.Aggregator;
+import com.avispl.symphony.dal.communicator.RestCommunicator;
+import com.avispl.symphony.dal.infrastructure.management.neat.pulse.common.PingMode;
+
+
+public class NeatPulseCommunicator extends RestCommunicator implements Aggregator, Monitorable, Controller {
+	/**
+	 * Process that is running constantly and triggers collecting data from Nureva Console SE API endpoints, based on the given timeouts and thresholds.
+	 *
+	 * @author Harry
+	 * @since 1.0.0
+	 */
+	class NurevaConsoleDataLoader implements Runnable {
+		private volatile boolean inProgress;
+		private volatile boolean flag = false;
+
+		public NurevaConsoleDataLoader() {
+			inProgress = true;
+		}
+
+		@Override
+		public void run() {
+			loop:
+			while (inProgress) {
+				try {
+					TimeUnit.MILLISECONDS.sleep(500);
+				} catch (InterruptedException e) {
+					// Ignore for now
+				}
+
+				if (!inProgress) {
+					break loop;
+				}
+
+				// next line will determine whether Nureva Console monitoring was paused
+				updateAggregatorStatus();
+				if (devicePaused) {
+					continue loop;
+				}
+				if (logger.isDebugEnabled()) {
+					logger.debug("Fetching other than aggregated device list");
+				}
+				long currentTimestamp = System.currentTimeMillis();
+				if (!flag && nextDevicesCollectionIterationTimestamp <= currentTimestamp) {
+					flag = true;
+				}
+
+				while (nextDevicesCollectionIterationTimestamp > System.currentTimeMillis()) {
+					try {
+						TimeUnit.MILLISECONDS.sleep(1000);
+					} catch (InterruptedException e) {
+						//
+					}
+				}
+
+				if (!inProgress) {
+					break loop;
+				}
+				if (flag) {
+					nextDevicesCollectionIterationTimestamp = System.currentTimeMillis() + 30000;
+					flag = false;
+				}
+
+				if (logger.isDebugEnabled()) {
+					logger.debug("Finished collecting devices statistics cycle at " + new Date());
+				}
+			}
+			// Finished collecting
+		}
+
+		/**
+		 * Triggers main loop to stop
+		 */
+		public void stop() {
+			inProgress = false;
+		}
+	}
+
+	/**
+	 * Indicates whether a device is considered as paused.
+	 * True by default so if the system is rebooted and the actual value is lost -> the device won't start stats
+	 * collection unless the {@link NeatPulseCommunicator#retrieveMultipleStatistics()} method is called which will change it
+	 * to a correct value
+	 */
+	private volatile boolean devicePaused = true;
+
+	/**
+	 * We don't want the statistics to be collected constantly, because if there's not a big list of devices -
+	 * new devices' statistics loop will be launched before the next monitoring iteration. To avoid that -
+	 * this variable stores a timestamp which validates it, so when the devices' statistics is done collecting, variable
+	 * is set to currentTime + 30s, at the same time, calling {@link #retrieveMultipleStatistics()} and updating the
+	 */
+	private long nextDevicesCollectionIterationTimestamp;
+
+	/**
+	 * This parameter holds timestamp of when we need to stop performing API calls
+	 * It used when device stop retrieving statistic. Updated each time of called #retrieveMultipleStatistics
+	 */
+	private volatile long validRetrieveStatisticsTimestamp;
+
+	/**
+	 * Update the status of the device.
+	 * The device is considered as paused if did not receive any retrieveMultipleStatistics()
+	 * calls during {@link NeatPulseCommunicator}
+	 */
+	private synchronized void updateAggregatorStatus() {
+		devicePaused = validRetrieveStatisticsTimestamp < System.currentTimeMillis();
+	}
+
+	/**
+	 * A mapper for reading and writing JSON using Jackson library.
+	 * ObjectMapper provides functionality for converting between Java objects and JSON.
+	 * It can be used to serialize objects to JSON format, and deserialize JSON data to objects.
+	 */
+	ObjectMapper objectMapper = new ObjectMapper();
+
+	/**
+	 * Executor that runs all the async operations, that is posting and
+	 */
+	private ExecutorService executorService;
+
+	/**
+	 * A private field that represents an instance of the NurevaConsoleLoader class, which is responsible for loading device data for Nureva Console
+	 */
+	private NurevaConsoleDataLoader deviceDataLoader;
+
+	/**
+	 * Private variable representing the local extended statistics.
+	 */
+	private ExtendedStatistics localExtendedStatistics;
+
+	/**
+	 * ping mode
+	 */
+	private PingMode pingMode = PingMode.ICMP;
+
+	/**
+	 * Retrieves {@link #pingMode}
+	 *
+	 * @return value of {@link #pingMode}
+	 */
+	public String getPingMode() {
+		return pingMode.name();
+	}
+
+	/**
+	 * Sets {@link #pingMode} value
+	 *
+	 * @param pingMode new value of {@link #pingMode}
+	 */
+	public void setPingMode(String pingMode) {
+		this.pingMode = PingMode.ofString(pingMode);
+	}
+
+	/**
+	 * Constructs a new instance of NurevaConsoleCommunicator.
+	 *
+	 * @throws IOException If an I/O error occurs while loading the properties mapping YAML file.
+	 */
+	public NeatPulseCommunicator() throws IOException {
+		this.setTrustAllCertificates(true);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * <p>
+	 *
+	 * Check for available devices before retrieving the value
+	 * ping latency information to Symphony
+	 */
+	@Override
+	public int ping() throws Exception {
+		if (this.pingMode == PingMode.ICMP) {
+			return super.ping();
+		} else if (this.pingMode == PingMode.TCP) {
+			if (isInitialized()) {
+				long pingResultTotal = 0L;
+
+				for (int i = 0; i < this.getPingAttempts(); i++) {
+					long startTime = System.currentTimeMillis();
+
+					try (Socket puSocketConnection = new Socket(this.host, this.getPort())) {
+						puSocketConnection.setSoTimeout(this.getPingTimeout());
+						if (puSocketConnection.isConnected()) {
+							long pingResult = System.currentTimeMillis() - startTime;
+							pingResultTotal += pingResult;
+							if (this.logger.isTraceEnabled()) {
+								this.logger.trace(String.format("PING OK: Attempt #%s to connect to %s on port %s succeeded in %s ms", i + 1, host, this.getPort(), pingResult));
+							}
+						} else {
+							if (this.logger.isDebugEnabled()) {
+								this.logger.debug(String.format("PING DISCONNECTED: Connection to %s did not succeed within the timeout period of %sms", host, this.getPingTimeout()));
+							}
+							return this.getPingTimeout();
+						}
+					} catch (SocketTimeoutException | ConnectException tex) {
+						throw new RuntimeException("Socket connection timed out", tex);
+					} catch (UnknownHostException ex) {
+						throw new UnknownHostException(String.format("Connection timed out, UNKNOWN host %s", host));
+					} catch (Exception e) {
+						if (this.logger.isWarnEnabled()) {
+							this.logger.warn(String.format("PING TIMEOUT: Connection to %s did not succeed, UNKNOWN ERROR %s: ", host, e.getMessage()));
+						}
+						return this.getPingTimeout();
+					}
+				}
+				return Math.max(1, Math.toIntExact(pingResultTotal / this.getPingAttempts()));
+			} else {
+				throw new IllegalStateException("Cannot use device class without calling init() first");
+			}
+		} else {
+			throw new IllegalArgumentException("Unknown PING Mode: " + pingMode);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<Statistics> getMultipleStatistics() throws Exception {
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void controlProperty(ControllableProperty controllableProperty) throws Exception {
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void controlProperties(List<ControllableProperty> controllableProperties) throws Exception {
+		if (CollectionUtils.isEmpty(controllableProperties)) {
+			throw new IllegalArgumentException("ControllableProperties can not be null or empty");
+		}
+		for (ControllableProperty p : controllableProperties) {
+			try {
+				controlProperty(p);
+			} catch (Exception e) {
+				logger.error(String.format("Error when control property %s", p.getProperty()), e);
+			}
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<AggregatedDevice> retrieveMultipleStatistics() throws Exception {
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<AggregatedDevice> retrieveMultipleStatistics(List<String> list) throws Exception {
+		return retrieveMultipleStatistics().stream().filter(aggregatedDevice -> list.contains(aggregatedDevice.getDeviceId())).collect(Collectors.toList());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * set API Key into Header of Request
+	 */
+	@Override
+	protected HttpHeaders putExtraRequestHeaders(HttpMethod httpMethod, String uri, HttpHeaders headers) {
+		return headers;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void authenticate() throws Exception {
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void internalInit() throws Exception {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Internal init is called.");
+		}
+		executorService = Executors.newFixedThreadPool(1);
+		executorService.submit(deviceDataLoader = new NurevaConsoleDataLoader());
+		super.internalInit();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void internalDestroy() {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Internal destroy is called.");
+		}
+		if (deviceDataLoader != null) {
+			deviceDataLoader.stop();
+			deviceDataLoader = null;
+		}
+		if (executorService != null) {
+			executorService.shutdownNow();
+			executorService = null;
+		}
+		if (localExtendedStatistics != null && localExtendedStatistics.getStatistics() != null && localExtendedStatistics.getControllableProperties() != null) {
+			localExtendedStatistics.getStatistics().clear();
+			localExtendedStatistics.getControllableProperties().clear();
+		}
+		nextDevicesCollectionIterationTimestamp = 0;
+		super.internalDestroy();
+	}
+}
---
+++ src/test/java/com/avispl/symphony/dal/infrastructure/management/neat/pulse/NeatPulseCommunicatorTest.java
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (c) 2024 AVI-SPL, Inc. All Rights Reserved.
+ */
+
+package com.avispl.symphony.dal.infrastructure.management.neat.pulse;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.avispl.symphony.api.dal.dto.control.ControllableProperty;
+import com.avispl.symphony.api.dal.dto.monitor.ExtendedStatistics;
+import com.avispl.symphony.api.dal.dto.monitor.aggregator.AggregatedDevice;
+
+/**
+ * NeatPulseCommunicatorTest
+ *
+ * @author Harry / Symphony Dev Team<br>
+ * Created on 9/8/2023
+ * @since 1.0.0
+ */
+public class NeatPulseCommunicatorTest {
+	private ExtendedStatistics extendedStatistic;
+	private NeatPulseCommunicator neatPulseCommunicator;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		neatPulseCommunicator = new NeatPulseCommunicator();
+		neatPulseCommunicator.setHost("");
+		neatPulseCommunicator.setLogin("");
+		neatPulseCommunicator.setPassword("");
+		neatPulseCommunicator.setPort(443);
+		neatPulseCommunicator.init();
+		neatPulseCommunicator.connect();
+	}
+
+	@AfterEach
+	void destroy() throws Exception {
+		neatPulseCommunicator.disconnect();
+		neatPulseCommunicator.destroy();
+	}
+}
